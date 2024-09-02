8eb1acb96eef126be93b30f369d0369254b68a43
Santiago Escobar <santiago.escobar30220@ucaldas.edu.co>
2024-09-01 19:46:22 -0500

    cambios septiembre 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-// js/script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Código JavaScript para interactividad
+    const imagenes = document.querySelectorAll('.carrusel-imagenes img');
+    let indiceActual = 0;
+
+    // Función para ocultar todas las imágenes excepto la primera
+    function ocultarImagenes() {
+        imagenes.forEach((img, indice) => {
+            img.style.display = (indice === indiceActual) 
+            ? 'block' 
+            : 'none';
+        });
+    }
+
+    // Inicialmente, ocultar todas las imágenes excepto la primera
+    ocultarImagenes();
+
+    // Función para cambiar a la siguiente imagen
+    function cambiarImagen() {
+        indiceActual = (indiceActual + 1) % imagenes.length; // Volver al inicio si es necesario
+        ocultarImagenes();
+    }
+
+    // Cambiar imagen cada 5 segundos
+    setInterval(cambiarImagen, 5000);
+
+
 });
+const a=[1,2,3,4,5,6,7,8,9,10]
+console.log(a)
